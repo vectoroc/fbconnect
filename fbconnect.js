b0fe512cc9fe385281c5251940e0d235129e2b83
Victor Grigoriev <vectoroc@gmail.com>
2009-11-27 17:59:10 +0300

    my current state

---
+++ fbconnect.js
@@ -0,0 +1,18 @@
+Drupal.behaviors.fbconnect = function(context) {
+	if (Drupal.settings.fbconnect && Drupal.settings.fbconnect.api_key) {
+		FB.Bootstrap.requireFeatures(["Connect","XFBML"], function() {
+			FB.Facebook.init(
+				Drupal.settings.fbconnect.api_key, 
+				Drupal.settings.basePath + Drupal.settings.fbconnect.xd_path
+			);
+			$(context).each(function() {
+				var elem = $(this).get(0);
+				FB.XFBML.Host.parseDomElement(elem);
+			});
+		});
+	}
+};
+
+function facebook_onlogin_ready() {
+	$("#fbconnect-autoconnect-form").submit();
+}
